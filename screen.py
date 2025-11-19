d5cc179bb342938ef7b4a33667ea12e35f7f0fb2
Michal Kozłowski <m1chol@proton.me>
2025-11-19 13:46:51 +0100

    add missing req

--- screen.py
+++ screen.py
@@ -1,17 +1,44 @@
 from luma.core.interface.serial import i2c
 from luma.oled.device import ssd1306
 from luma.core.render import canvas
+from PIL import ImageDraw, Image
+import socket
 from time import sleep
+import os
 
-serial = i2c(port=1, address=0x3C)
+serial = i2c(port=7, address=0x3C)
 device = ssd1306(serial, width=128, height=32)
 
-text = "Hello World"
+
+def get_local_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(("8.8.8.8", 80))
+        ip = s.getsockname()[0]
+    except OSError:
+        ip = None
+    finally:
+        s.close()
+    return ip
+
 
 with canvas(device) as draw:
-    w, h = draw.textsize(text)  # default built-in font
-    x = (device.width - w) // 2
-    y = (device.height - h) // 2
-    draw.text((x, y), text, fill=255)
+    draw.text(
+        (device.width // 2, device.height // 2),
+        "Waiting for network...",
+        fill=255,
+        anchor="mm",
+    )
 
-sleep(20)
+    ip = None
+    while not ip:
+        ip = get_local_ip()
+        sleep(0.5)
+
+
+img = Image.new("1", device.size)
+draw = ImageDraw.Draw(img)
+draw.text((device.width // 2, device.height // 2), ip, fill=255, anchor="mm")
+device.display(img)
+
+os._exit(0)
