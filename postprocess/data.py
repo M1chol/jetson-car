b7918e8eb739c198d486343ed98d3fff912ffa26
Michal Kozłowski <m1chol@proton.me>
2025-11-28 21:05:42 +0100

    path creation v1

--- postprocess/data.py
+++ postprocess/data.py
@@ -3,6 +3,9 @@
 import json
 import csv
 import pandas as pd
+import numpy as np
+
+TIMESTEP = 0.1
 
 # Load config
 print("[CAR] Loading config file...")
@@ -26,12 +29,11 @@ def empty_record():
             "speed_front_left": None,
             "current_front_left": None,
             "acceleration_front_left": None,
-            "front_angle": None,
-            "back_angle": None,
+            "angle_front": None,
+            "angle_rear": None,
         }
 
 def normalizeData(fileIn: str, fileOut: str) -> None:
-    TIMESTEP = 0.02
     timeline = {}
     keys = list(empty_record().keys())
     with open(fileIn, "r") as f:
@@ -71,15 +73,15 @@ def normalizeData(fileIn: str, fileOut: str) -> None:
                     timeline[ts][keys[motor+2]] = (entry["data"]["act"] + timeline[ts][keys[motor+2]]) / 2
 
             elif entry["type"] == "SERVO":
-                if not timeline[ts]["front_angle"]:
-                    timeline[ts]["front_angle"] = float(entry["data"]["servo_2"])
+                if not timeline[ts]["angle_front"]:
+                    timeline[ts]["angle_front"] = float(entry["data"]["servo_2"])
                 else:
-                    timeline[ts]["front_angle"] = (float(entry["data"]["servo_2"]) + timeline[ts]["front_angle"]) / 2
+                    timeline[ts]["angle_front"] = (float(entry["data"]["servo_2"]) + timeline[ts]["angle_front"]) / 2
                 
-                if not timeline[ts]["back_angle"]:
-                    timeline[ts]["back_angle"] = float(entry["data"]["servo_1"])
+                if not timeline[ts]["angle_rear"]:
+                    timeline[ts]["angle_rear"] = float(entry["data"]["servo_1"])
                 else:
-                    timeline[ts]["back_angle"] =(float(entry["data"]["servo_1"]) + timeline[ts]["back_angle"]) / 2
+                    timeline[ts]["angle_rear"] =(float(entry["data"]["servo_1"]) + timeline[ts]["angle_rear"]) / 2
                 
     with open(fileOut, "w", newline="") as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=["timestep"] + list(empty_record().keys()))
@@ -96,9 +98,39 @@ def normalizeData(fileIn: str, fileOut: str) -> None:
 def interpolate(fileIn: str, fileOut: str) -> None:
     df = pd.read_csv(fileIn)
     df = df.interpolate()
+    while not df.empty and df.iloc[0].isna().any():
+        df = df.iloc[1:]
     df.to_csv(fileOut, index=False)
     print(f"Interpolate done. CSV saved to: {fileOut}")
 
+def calculatePath(fileIn: str, fileOut: str) -> None:
+    df = pd.read_csv(fileIn)
+    df["speed_mean"] = (- df["speed_front_right"] - df["speed_rear_right"] + df["speed_rear_left"] + df["speed_front_left"]) / 4
+    df["speed_mps_mean"] = df["speed_mean"] / 10 * 2 * np.pi * config["WHEEL_RADIUS"] / 60
+    x, y, theta = 0.0, 0.0, 0.0
+    df["angle_mean"] = (df["angle_front"] - df["angle_rear"]) / 2 + 1
+    df["angle_mean_rad"] = np.deg2rad(df["angle_mean"])
+    pos_x = []
+    pos_y = []
+    for _, row in df.iterrows():
+        # v / L * 2 * dt * tan()
+        theta += row["speed_mps_mean"] / config["DIFF_LENGTH"] * 2 * TIMESTEP * np.tan(row["angle_mean_rad"])
+        x += row["speed_mps_mean"] * np.cos(theta) * TIMESTEP
+        y += row["speed_mps_mean"] * np.sin(theta) * TIMESTEP
+        pos_x.append(x)
+        pos_y.append(y)
+
+    path = pd.DataFrame({
+        "pos_x": pos_x,
+        "pos_y": pos_y,
+        "current_speed": df["speed_mps_mean"],
+        "current_angle": df["angle_mean_rad"],
+        "timestep": df["timestep"]
+    })
+
+    path.to_csv(fileOut, index=False)
+
 if __name__ == "__main__":
-    normalizeData("results/2025-11-24/out.txt", "results/2025-11-24/normalized.csv")
-    interpolate("results/2025-11-24/normalized.csv", "results/2025-11-24/interpolated.csv")
+    normalizeData("results/2025-11-25/out.txt", "results/2025-11-25/normalized.csv")
+    interpolate("results/2025-11-25/normalized.csv", "results/2025-11-25/interpolated.csv")
+    calculatePath("results/2025-11-25/interpolated.csv", "results/2025-11-25/path.csv")
--- postprocess/drawCharts.py
+++ postprocess/drawCharts.py
@@ -31,6 +31,15 @@ def drawSpeed(interpolated, raw):
     fig.tight_layout()
     plt.show()
 
+def drawSpeedAll(interpolated):
+    plt.figure(figsize=(10, 6))
+
+    plt.plot(interpolated["timestep"], -interpolated["speed_rear_right"], marker="o", markersize=2)
+    plt.plot(interpolated["timestep"], -interpolated["speed_front_right"], marker="o", markersize=2)
+    plt.plot(interpolated["timestep"], interpolated["speed_rear_left"], marker="o", markersize=2)
+    plt.plot(interpolated["timestep"], interpolated["speed_front_left"], marker="o", markersize=2)
+    plt.show()
+
 def drawCurrent(interpolated, raw):
     fig, ax = plt.subplots(2, 2, figsize=(10, 6))
 
@@ -62,12 +71,12 @@ def drawCurrent(interpolated, raw):
 def drawAngle(interpolated, raw):
     fig, ax = plt.subplots(1, 2, figsize=(10, 4))
 
-    ax[0].plot(interpolated["timestep"], interpolated["front_angle"], marker="o", markersize=2)
-    ax[0].scatter(raw["timestep"], raw["front_angle"], s=10, color="tab:red")
+    ax[0].plot(interpolated["timestep"], interpolated["angle_front"], marker="o", markersize=2)
+    ax[0].scatter(raw["timestep"], raw["angle_front"], s=10, color="tab:red")
     ax[0].set_title("Front Angle")
 
-    ax[1].plot(interpolated["timestep"], interpolated["back_angle"], marker="o", markersize=2)
-    ax[1].scatter(raw["timestep"], raw["back_angle"], s=10, color="tab:red")
+    ax[1].plot(interpolated["timestep"], interpolated["angle_rear"], marker="o", markersize=2)
+    ax[1].scatter(raw["timestep"], raw["angle_rear"], s=10, color="tab:red")
     ax[1].set_title("Back Angle")
 
     # Set axis labels for both subplots
@@ -78,9 +87,51 @@ def drawAngle(interpolated, raw):
     fig.tight_layout()
     plt.show()
 
+def drawAngleAll(interpolated):
+    plt.figure(figsize=(10, 6))
+    plt.plot(interpolated["timestep"], -interpolated["angle_front"], marker="o", markersize=2)
+    plt.plot(interpolated["timestep"], interpolated["angle_rear"], marker="o", markersize=2)
+    plt.show()
+
+def drawMapInputs(path):
+    plt.figure(figsize=(10, 6))
+    plt.title("Średnia prędkość")
+    plt.plot(path["timestep"], path["current_speed"])
+    plt.show()
+    plt.title("Średni kąt skrętu")
+    plt.plot( path["timestep"], path["current_angle"])
+    plt.show()
+
+def drawMap(path):
+    plt.figure(figsize=(10, 6))  # Adjust figure size
+    plt.plot(path["pos_x"], path["pos_y"], 'o-', label="Path")  # 'o-' means points + lines
+    
+    for i, row in path.iterrows():
+        if i % 30 == 0:
+            plt.text(
+                row["pos_x"],
+                row["pos_y"],
+                f"{row['current_speed']:.1f}",  # Format speed (1 decimal)
+                fontsize=9,
+                ha='right',  # Horizontal alignment
+                va='bottom'  # Vertical alignment
+            )
+    
+    plt.title("Path with Speed Labels")
+    plt.axis('equal')
+    plt.xlabel("X Position")
+    plt.ylabel("Y Position")
+    plt.grid(True)
+    plt.legend()
+    plt.show()
+
 if __name__ == "__main__":
-    interpolated = load("results/2025-11-24/interpolated.csv")
-    raw = load("results/2025-11-24/normalized.csv")
-    drawSpeed(interpolated, raw)
-    drawCurrent(interpolated, raw)
-    drawAngle(interpolated, raw)
+    interpolated = load("results/2025-11-25/interpolated.csv")
+    raw = load("results/2025-11-25/normalized.csv")
+    path = load("results/2025-11-25/path.csv")
+    drawSpeedAll(interpolated)
+    drawAngleAll(interpolated)
+    # drawCurrent(interpolated, raw)
+    #drawAngle(interpolated, raw)
+    drawMapInputs(path)
+    drawMap(path)
