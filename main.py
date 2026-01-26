cc898a33d7b97556f9b6aece3b405b172b557458
Michal Kozłowski <m1chol@proton.me>
2026-01-26 19:58:46 +0100

    fix: rework argparse for dashboard

--- main.py
+++ main.py
@@ -5,15 +5,18 @@ import argparse
 parser = argparse.ArgumentParser(description="Jetson powered autonomus car")
 parser.add_argument("--debug", action="store_true", help="Enable debug mode")
 parser.add_argument("--save", action="store_true", help="Save data to persisted file")
-parser.add_argument("--dashboard", help="to enable web dashboard pass media server path", default="/home/m1/Downloads/media")
+parser.add_argument("--dashboard", actuion="store_true", help="Enable web dashboard default media path /home/m1/Downloads/media")
+parser.add_argument("--dashboard-dir", help="pass different media server path")
 args = parser.parse_args()
 config = None
 
 if args.dashboard: 
-    from dashboard import start_dashboard, start_media_server
-    dashboard_thread = Thread(target=start_dashboard, daemon=True)
-    start_media_server(args.dashboard)
+    import dashboard as ds
+    dashboard_thread = Thread(target=ds.start_dashboard, daemon=True)
+    if args.dashboard_dir:
+        ds.start_media_server(args.dashboard_dir)
+    else:
+        ds.start_media_server("/home/m1/Downloads/media")
 
 if not start(persistRun=args.save, debug=args.debug):
     print("[MAIN] Car setup failed, exiting")
-
